1f2814cf2344bee7fee92aa10b56cde31d9ddfc8
chishin <pro@xxgzs.org>
2022-05-19 11:36:02 +0800

    增加配置文件初始化

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"os"
 	_ "time/tzdata"
 
 	"github.com/xiaoxinpro/speedtest-go-zh/config"
@@ -14,14 +17,57 @@ import (
 )
 
 var (
-	optConfig = flag.String("c", "", "config file to be used, defaults to settings.toml in the same directory")
+	optConfig = flag.String("c", "./config/settings.toml", "config file to be used, defaults to settings.toml in the same directory")
 )
 
 func main() {
+	InitFile()
 	flag.Parse()
 	conf := config.Load(*optConfig)
 	web.SetServerLocation(&conf)
 	results.Initialize(&conf)
 	database.SetDBInfo(&conf)
 	log.Fatal(web.ListenAndServe(&conf))
+}
+
+//InitFile 初始化文件
+func InitFile() {
+	if isOK,_ := PathExists("./config"); isOK == false {
+		os.MkdirAll("./config", os.ModePerm)
+	}
+	if isOK,_ := PathExists("./config/settings.toml"); isOK == false {
+		CopyFile("./settings.toml", "./config/settings.toml")
+		os.Chmod("./config/settings.toml", os.ModePerm)
+	}
+}
+
+//PathExists 判断一个文件或文件夹是否存在
+func PathExists(path string) (bool,error) {
+	_,err := os.Stat(path)
+	if err == nil {
+		return true,nil
+	}
+	if os.IsNotExist(err) {
+		return false,nil
+	}
+	return false,err
+}
+
+//CopyFile 复制文件
+func CopyFile(srcFileName string, dstFileName string) (written int64, err error) {
+	srcFile, err := os.Open(srcFileName)
+	if err != nil {
+		fmt.Printf("open file err = %v\n", err)
+		return
+	}
+	defer srcFile.Close()
+
+	//open dstFileName
+	dstFile, err := os.OpenFile(dstFileName, os.O_WRONLY | os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("open file err = %v\n", err)
+		return
+	}
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
 }
